07bfc1a19f2de4af8755fdc9b69263bb878f0b54
Student <vivu9928@colorado.edu>
2025-11-09 10:46:17 -0700

    new changes

--- proxy.c
+++ proxy.c
@@ -16,6 +16,7 @@
 #include <sys/select.h>
 #include <sys/time.h>
 #include <stdatomic.h>
+
 // #include "proxy.h"
 
 #define BUFFER_SIZE 8192
@@ -32,10 +33,57 @@ typedef struct {
     struct sockaddr_in server_addr;
 } server_info_t;
 
+typedef struct {
+    int portno; 
+    char *hostname;
+    char *f_path; 
+    char *msg_body; 
+} req_info; 
+
 client_info_t clients[MAX_CONNECTIONS];
 atomic_int client_count = 0;
 
 server_info_t server_info;
+
+
+int parse_request(char * request, req_info * request_info) {
+    req_info->f_path = strtok(request, "\t\r\n");
+    req_info->hostname = strtok(NULL, "\t\r\n"); 
+    req_info->portno = 80; 
+}
+
+int send_request(req_info *request_info) { 
+    char response[1024]; 
+    int body_len = strlen(msg_body)
+}
+
+int handle_request(int clientf) {   
+    char buffer[BUFFER_SIZE] = {0}; 
+    char request_copy[BUFFER_SIZE] = {0}; 
+
+    ssize_t bytes_read = read(clientf, buffer, sizeof(buffer) - 1);
+
+    if (bytes_read == 0) {
+        printf("Client close connection\n");
+    }
+    else if (bytes_read < 0) { 
+        printf("Read error: %s\n", strerror(errno));
+    }
+
+    buffer[bytes_read] = '\0';
+    strcpy(request_copy, buffer); 
+    printf("Received request: \n%s \n", buffer); 
+
+    req_info request_info; 
+
+    if (parse_request(buf, &req_info) == 0){ 
+        printf("Request infoooooormation succesffully parsed");    
+    }
+    else { 
+        printf("Invalid info format or error parsing"); 
+        exit(0);
+    }
+}
 
 
 
@@ -47,8 +95,9 @@ int main(int argc, char **argv) {
     struct sockaddr_in serveraddr;
     struct hostent *server;
     char buffer[BUFFER_SIZE];
+    int ttl; 
 
-    if (argc != 2) {
+    if (argc < 2) {
         fprintf(stderr, "usage: %s <port>\n", argv[0]); 
         exit(1); 
     }
@@ -58,6 +107,10 @@ int main(int argc, char **argv) {
     if (sockfd < 0) {
         perror("ERROR opening socket");
         exit(1);
+    }
+
+    if (argc  > 2) { 
+            ttl = atoi(argv[2]); 
     }
 
     memset((char *) &serveraddr, 0, sizeof(serveraddr));
@@ -94,8 +147,7 @@ int main(int argc, char **argv) {
 
         if (process_id == 0) { 
             close(sockfd);
-            // Handle client connection in child process
-            // (Proxy logic would go here)
+            handle_request()
             close(clients[client_count].client_socket);
             exit(0);
         } else { 
